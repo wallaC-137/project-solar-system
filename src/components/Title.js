e2e487567529545b17cbaf844910d7a6e3a044a4
Wallace <wallace_031@hotmail.com>
2022-11-25 15:45:05 -0300

    correção lint

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropType from 'prop-types';
 
 class Title extends React.Component {
   render() {
     const { headline } = this.props;
+
     return (
-      <h2> { headline } </h2>
+      <h2>
+        { headline }
+      </h2>
     );
   }
 }
+
+Title.propTypes = {
+  headline: PropType.string.isRequired,
+};
 
 export default Title;
